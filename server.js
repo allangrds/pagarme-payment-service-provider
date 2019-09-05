f45420b3bde89088a99d17e4345e71510ad4fdde
Allan Ramos <allangrds@gmail.com>
2019-09-05 12:37:58 -0300

    feat(cors): add cors lib

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
 
 const app = express()
 const port = 3002
@@ -10,6 +11,7 @@ app.use(logger('dev'))
 app.disable('x-powered-by')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.get('/', (req, res) => res.status(200).json({ message: 'Hello World' }))
 
