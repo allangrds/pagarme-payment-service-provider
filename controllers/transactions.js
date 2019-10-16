d00ea2023757ce86f81b59639287e204851e4731
Allan Ramos <allangrds@gmail.com>
2019-10-15 22:29:05 -0300

    transactions: update returned values

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -6,11 +6,12 @@ const index = (req, res) => {
     results: [
       {
         id: 'trs_1234',
+        amount: 1000,
         description: 'Essa é uma transação',
         payment_method: 'debit_card',
         last_card_digits: '0569',
         card_holder_name: 'João da Silva Sauro',
-        card_validation: '0520',
+        card_expiration_date: '0520',
         created_at: '2019-10-18-10:05:55',
       },
     ],
@@ -22,15 +23,31 @@ const index = (req, res) => {
 const show = (req, res) => {
   const transaction = {
     id: 'trs_1234',
+    amount: 1000,
     description: 'Essa é uma transação',
     payment_method: 'debit_card',
     last_card_digits: '0569',
     card_holder_name: 'João da Silva Sauro',
-    card_validation: '0520',
+    card_expiration_date: '0520',
     created_at: '2019-10-18-10:05:55',
   }
 
   return res.status(200).json(transaction)
 }
 
-module.exports = { index, show }
+const create = (req, res) => {
+  const transaction = {
+    id: 'trs_1234',
+    amount: 1000,
+    description: 'Essa é uma transação',
+    payment_method: 'debit_card',
+    last_card_digits: '0569',
+    card_holder_name: 'João da Silva Sauro',
+    card_expiration_date: '0520',
+    created_at: '2019-10-18-10:05:55',
+  }
+
+  return res.status(201).json(transaction)
+}
+
+module.exports = { index, show, create }
