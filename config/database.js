a87f33d74a39372f8b01d836a25bc78f3d775ac8
Allan Ramos <allangrds@gmail.com>
2020-01-20 20:17:43 -0300

    chore(ci): update ci to run tests

--- config/database.js
+++ config/database.js
@@ -1,35 +1,11 @@
 require('dotenv').config()
 
 const database = {
-  development: {
+  [process.env.NODE_ENV]: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    host: 'database',
-    dialect: 'mysql',
-    operatorsAliases: false,
-    dialectOptions: {
-      useUTC: false,
-    },
-    timezone: '-03:00',
-  },
-  test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: 'database',
-    dialect: 'mysql',
-    operatorsAliases: false,
-    dialectOptions: {
-      useUTC: false,
-    },
-    timezone: '-03:00',
-  },
-  production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    host: 'database',
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql',
     operatorsAliases: false,
     dialectOptions: {
