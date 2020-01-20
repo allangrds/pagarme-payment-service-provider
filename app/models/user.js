10b8c900f0f0dbc259c66a4f9985114edbd5cf50
Allan Ramos <allangrds@gmail.com>
2020-01-20 20:03:06 -0300

    feat(user): update uuid generation logic

--- app/models/user.js
+++ app/models/user.js
@@ -1,14 +1,25 @@
 const bcrypt = require('bcryptjs')
+const uuidv4 = require('uuid/v4')
+
+function getRandomArbitrary (min, max) {
+  return Math.random() * (max - min) + min
+}
 
 module.exports = (sequelize, DataTypes) => {
+  const hooks = {
+    beforeCreate: (user) => {
+      const uuidString = user.password + getRandomArbitrary(0, 100)
+
+      user.api_key = uuidv4(uuidString) // eslint-disable-line no-param-reassign
+      user.password = bcrypt.hashSync(user.password, 10) // eslint-disable-line no-param-reassign
+    },
+  }
+
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-  })
-
-  User.beforeCreate(async (user) => {
-    user.password = bcrypt.hashSync(user.password, 10) // eslint-disable-line no-param-reassign
-  })
+    api_key: DataTypes.STRING,
+  }, { hooks })
 
   return User
 }
